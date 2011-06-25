70db7bf1ac291bb0a651b753344a89eb33983fbc
tristen <tristen@tristen.ca>
2011-06-25 19:00:07 -0400

    Initial commit

---
+++ scripts/data.js
@@ -0,0 +1,20 @@
+var stations = [
+  {
+    'station' : 'cfbx',
+    'count' : 1190,
+    'location': 'Kamloops, BC',
+    'cdn_play': 746,
+    'uk_play': 28,
+    'us_play': 376,
+    'int_play': 39
+  },
+  {
+    'station' : 'cfmh',
+    'count' : 108,
+    'location': 'Saint John, NB',
+    'cdn_play': 31,
+    'uk_play': 30,
+    'us_play': 38,
+    'int_play': 9
+  }
+];
---
+++ scripts/site.js
@@ -0,0 +1,72 @@
+var m, o;
+var stationOverlay = function (item) {
+    $('<a></a>').addClass('station-point ' + item.station).appendTo('#canada').click(function () {
+        $('.station-point').removeClass('active');
+        var content = '<h2>' + item.station + '<h2>'
+                    + 'Songs Tracked: ' + item.count
+                    + '<h3> Where </h3>'
+                    + 'Canada: ' + item.cdn_play
+                    + 'USA: ' + item.us_play
+                    + 'United Kingdom: ' + item.uk_play
+                    + 'International: ' + item.int_play;
+        $('#station').html(content);
+        $('#station').show();
+        $(this).addClass('active');
+        o.setProvider(new com.modestmaps.WaxProvider({
+            baseUrl: 'http://a.tiles.mapbox.com/tristen/',
+            layerName: item.station,
+            filetype: '.png',
+            zoomRange: [4, 8]
+        }));
+    });
+};
+
+var mapSetup = function () {
+        var mm = com.modestmaps;
+
+        m = new mm.Map('map', new com.modestmaps.WaxProvider({
+            baseUrl: 'http://a.tiles.mapbox.com/tristen/',
+            layerName: 'base_layer',
+            zoomRange: [4, 8]
+        }));
+        m.setCenterZoom(
+        new com.modestmaps.Location(50, -80), 4);
+
+        o = new mm.Map('map-overlay', new com.modestmaps.WaxProvider({
+            baseUrl: 'http://a.tiles.mapbox.com/tristen/',
+            layerName: 'cfbx',
+            zoomRange: [4, 8]
+        })).zoomer().interaction();
+        o.setCenterZoom(
+        new com.modestmaps.Location(50, -80), 4);
+
+        o.addCallback('drawn', function (modestmap, e) {
+            m.setCenterZoom(o.getCenter(), o.getZoom());
+        });
+    };
+
+$(function () {
+    _(stations).map(stationOverlay);
+    mapSetup();
+    $('a.cfbx').trigger('click');
+
+    $('div#map-overlay').bind('addedtooltip', function (e, tooltip, context, evt) {
+        var $tooltip = $(tooltip);
+        var $mapdiv = $('div#map');
+        // Position far outside of the page in order for it to get a height
+        $tooltip.offset({ top: -10000 });
+        $tooltip.bind('selectstart', function (e) {
+            e.stopPropagation();
+        });
+        $tooltip.append('<div class="arrowsprite">&nbsp;</div>');
+        $('body').append($tooltip);
+        // Reposition correctly the first time
+        $tooltip.offset({
+            top: evt.pageY - $tooltip.outerHeight(true) - 10,
+            left: evt.pageX - ($tooltip.outerWidth(true) / 2),
+            'z-index': 100
+        });
+        // continue to follow the mouse.
+        return true;
+    });
+});
